9be69e8d74b43984cb8c3adbbe3db91649183d09
Becky Bell <rlbell@alum.bu.edu>
2014-07-11 16:56:00 -0400

    created randomly generate groups

--- groupmaker.rb
+++ groupmaker.rb
@@ -1,1 +1,6 @@
 people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
+
+puts "How large is your group?"
+how_large_of_a_group = gets.chomp.to_i
+random_group = people.sample(how_large_of_a_group)
+p random_group
